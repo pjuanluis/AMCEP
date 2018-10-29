ff5914dbf1652e0660e1d895035e6728449a4f0f
PEREZ RAMIREZ JUAN LUIS <Z043812@isban.mx>
2018-10-29 16:55:57 -0600

    Se iguala proyecto al del servidor

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class web extends CI_Controller {
+class Web extends CI_Controller {
 
 	function  __construct() {
 		parent:: __construct();	
